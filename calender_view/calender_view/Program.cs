9b8073401bfab3a87717fb1a0b82c43bac2e835e
Saya <107242326+sayasingh@users.noreply.github.com>
2024-11-25 15:17:48 +0545

    Add files via upload

---
+++ calender_view/calender_view/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calender_view
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int weeks = 5;
+            int daysInWeek = 7;
+            int[][] calendar = new int[weeks][];
+
+            for (int i = 0; i < weeks; i++)
+            {
+                calendar[i] = new int[daysInWeek];
+            }
+
+            int day = 1;
+            for (int i = 0; i < weeks; i++)
+            {
+                for (int j = 0; j < daysInWeek; j++)
+                {
+                    if (day <= 30)
+                    {
+                        calendar[i][j] = day++;
+                    }
+                    else
+                    {
+                        calendar[i][j] = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine("Sun Mon Tue Wed Thurs Fri Sat");
+            for (int i = 0; i < weeks; i++)
+            {
+                for (int j = 0; j < daysInWeek; j++)
+                {
+                    if (calendar[i][j] != 0)
+                    {
+                        Console.Write($"{calendar[i][j],3} ");
+                    }
+                    else
+                    {
+                        Console.Write("    ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ calender_view/calender_view/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("calender_view")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calender_view")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2de12c9-49c7-453c-b14a-42565a021d25")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
