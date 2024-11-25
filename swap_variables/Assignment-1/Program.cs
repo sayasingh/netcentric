31e886a12de3238c0aa6cfa7e6c92d0f495bd75b
Saya <107242326+sayasingh@users.noreply.github.com>
2024-11-25 15:16:53 +0545

    Add files via upload

---
+++ swap_variables/Assignment-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+            int y = 8;
+
+            x = x + y;
+            y = x - y;
+            x = x - y;
+            Console.WriteLine("x=" + x + ",y=" + y);
+        }
+    }
+}
